5d9996555e96b1ac50ff916124f1e7de9e34a15b
Alex Cornforth <alexcornforth@me.com>
2022-10-27 15:34:41 +0300

    Added template swift package

---
+++ README.md
@@ -0,0 +1,3 @@
+# wallet-ios-sdk
+
+A description of this package.
---
+++ Sources/wallet-ios-sdk/wallet_ios_sdk.swift
@@ -0,0 +1,6 @@
+public struct wallet_ios_sdk {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
